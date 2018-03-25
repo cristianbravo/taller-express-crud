2e67a6ea06ab3f56aa657540eba6cbcf665597a3
cristian bravo <cristian.bravo@catena.cl>
2018-03-25 16:35:14 -0300

    Definición inicial de la API REST

--- routes/api_users.js
+++ routes/api_users.js
@@ -1,9 +1,21 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
+// REST API to model Usuario
+
+/* Handle GET requests for Usuario's model listing. */
 router.get('/', function(req, res, next) {
-  res.render('index', {title: 'Taller ExpressJS'});
+  res.json({total: 0, data : [{}], info : "Ok"});
+});
+
+/* Handle post request to create Usuario's model. */
+router.post('/', function(req, res, next) {
+  res.json({total: 0, data : null, info : "Ok"});
+});
+
+/* Handle GET requests to looking an Usuario referenced by ID. */
+router.get('/:id', function(req, res, next) {
+  res.json({total: 0, data : [{}], info : "Ok recibiendo el parametro ID con valor " + req.params.id});
 });
 
 module.exports = router;
